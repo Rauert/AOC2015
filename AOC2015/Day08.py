f065282e68577ced3ee5432409330301e42d58e8
michelle.rauert <michelle.rauert@tnlc.com.au>
2019-01-16 16:01:44 +0800

    Day 8 Complete

--- AOC2015/Day08.py
+++ AOC2015/Day08.py
@@ -1,20 +1,49 @@
 import os, re
 fileDir = os.path.dirname(os.path.abspath(__file__))   # Directory of the Module
 input = open(fileDir+"\Inputs\AOC08_1.txt", "r").read().splitlines()
+#input = open(fileDir+"\Test\AOC08_1.txt", "r").read().splitlines()
 
+#Decode
 def Part1():
-    count = 0
+    totCount = 0
     for i in input:
-        print(i)
-        print(i.count("\\\\"))
-        print(i.count("\\\""))
-        print(i.count("\\x"))
-        #consider overlapping
-    print("Part 1:",count)
+        count = len(i)-2
+        #print(i)
+        j = 1
+        while j < len(i)-2:
+            #print(i[j:j+2],"\\\\","\\\"","\\x")
+            if i[j:j+2] in ["\\\\","\\\""]:
+                count -= 1
+                j += 1
+            elif i[j:j+2] == "\\x":
+                count -= 3
+                j += 3
+            j+=1
+        #print(len(i), count)
+        totCount += len(i) - count
+    #1892, too high
+    print("Part 1:",totCount)
 
+#Encode
 def Part2():
-
-    print("Part 2:")
+    totCount = 0
+    for i in input:
+        count = len(i)+4
+        #print(i)
+        j = 1
+        while j < len(i)-2:
+            #print(i[j:j+2],"\\\\","\\\"","\\x")
+            if i[j:j+2] in ["\\\\","\\\""]:
+                count += 2
+                j += 1
+            elif i[j:j+2] == "\\x":
+                count += 1
+                j += 3
+            j+=1
+        #print(len(i), count)
+        totCount += count - len(i)
+    #1892, too high
+    print("Part 2:",totCount)
 
 Part1()
 Part2()
